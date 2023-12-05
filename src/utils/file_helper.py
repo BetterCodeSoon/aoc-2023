0ac5086751cd200e692dc8eb47453e676e680e6e
BetterCodeSoon <notreally@somemail.com>
2023-12-05 16:22:49 +0100

    Established naming convention for puzzle input files.

--- src/utils/file_helper.py
+++ src/utils/file_helper.py
@@ -4,8 +4,20 @@ CURRENT_FILE = Path(__file__)
 PROJECT_ROOT = CURRENT_FILE.parent.parent
 
 
-def get_puzzle_input_path(day, filename):
-    return PROJECT_ROOT / "resources" / f"day{day}" / filename
+def get_puzzle_filename(day, puzzle_nr):
+    return f"day{day}_puzzle{puzzle_nr}_input.txt"
+
+
+def get_puzzle_input_path(day, puzzle_nr):
+    """
+    Naming convention for the files containing the puzzle inputs: \n
+        resources \n
+        ├── day{day} \n
+        │   ├─ day{day}_puzzle{puzzle_nr}_input.txt \n
+
+    example: day1_puzzle1_input.txt
+    """
+    return PROJECT_ROOT / "resources" / f"day{day}" / get_puzzle_filename(day, puzzle_nr)
 
 
 def read_file_lines(filepath):
--- tests/utils/test_file_helper.py
+++ tests/utils/test_file_helper.py
@@ -2,11 +2,18 @@ import src.utils.file_helper as file_helper
 from tests.conftest import testfile_path
 
 
+def test_get_puzzle_filename():
+    n = 1
+    expected = f"day{n}_puzzle{n}_input.txt"
+    assert expected == file_helper.get_puzzle_filename(n, n)
+
+
 def test_get_puzzle_input_path():
     day = 1
-    filename = "day1_puzzle1_input.txt"
+    puzzle_nr = 1
+    filename = file_helper.get_puzzle_filename(day, puzzle_nr)
     expected_path = file_helper.PROJECT_ROOT / "resources" / f"day{day}" / filename
-    assert expected_path == file_helper.get_puzzle_input_path(day, filename)
+    assert expected_path == file_helper.get_puzzle_input_path(day, puzzle_nr)
 
 
 def test_read_file_lines(testfile_path):
