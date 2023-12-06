b3d172e7f205671a1ffac41f97a578e0502822ae
BetterCodeSoon <notreally@somemail.com>
2023-12-06 01:36:32 +0100

    Refactored tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,7 @@ import src.utils.file_helper as file_helper
 @pytest.fixture
 def testfile_path():
     return file_helper.PROJECT_ROOT / "resources" / "tests" / "testfile1.txt"
+
+@pytest.fixture
+def testfile_content():
+    return ['1abc2:12\n', 'pqr3stu8vwx:38\n', 'a1b2c3d4e5f:15\n', 'treb7uchet:77']
--- tests/utils/test_file_helper.py
+++ tests/utils/test_file_helper.py
@@ -1,7 +1,18 @@
+import pytest
+
 import src.utils.file_helper as file_helper
+from tests.conftest import testfile_content
 from tests.conftest import testfile_path
 
 
+# @pytest.mark.parametrize(
+#     ("expected_key", "expected_val"),
+#     [
+#         ("1abc2", "12"),
+#         ("pqr3stu8vwx", "38"),
+#         ("a1b2c3d4e5f", "15"),
+#         ("treb7uchet", "77")
+#     ])
 class TestFileHelper:
     def test_get_puzzle_filename(self):
         n = 1
@@ -15,6 +26,10 @@ class TestFileHelper:
         expected_path = file_helper.PROJECT_ROOT / "resources" / f"day{day}" / filename
         assert expected_path == file_helper.get_puzzle_input_path(day, puzzle_nr)
 
-    def test_read_file_lines(self, testfile_path):
+    def test_read_file_lines(self, testfile_path, testfile_content):
         file_content = file_helper.read_file_lines(testfile_path)
-        assert file_content == ['123\n', '\n', '456\n', '789']
+        assert file_content == testfile_content
+
+    @pytest.mark.skip("yet to be implemented")
+    def test_read_expected_values(self, testfile_path):
+        pass
