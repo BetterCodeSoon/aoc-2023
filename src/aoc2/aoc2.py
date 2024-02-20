65915c12d9760ec092a40628abee2926ff1716cb
BetterCodeSoon <bettercodesoon@proton.me>
2024-02-20 22:59:44 +0100

    Implemented correct solution for part 2 of the Day 2 puzzle.

--- src/aoc2/aoc2.py
+++ src/aoc2/aoc2.py
@@ -5,11 +5,13 @@ from src.aoc2.elf_game import ElfGame
 
 
 def evaluate_game(elf_game: ElfGame, bag: Bag) -> bool:
+    # toDo check and remove redudant function and test
     return elf_game.possible_game(bag)
 
 
 if __name__ == '__main__':
     id_sum = 0
+    power_sum = 0
     game_bag = Bag(12, 13, 14)
     puzzle_path = file_helper.puzzle_input_path(2, 1)
     file_lines = file_helper.read_file_lines(puzzle_path)
@@ -19,8 +21,13 @@ if __name__ == '__main__':
         game_id = input_helper.read_game_id(game_str)
         game_sets = input_helper.read_game_sets_list(game_sets_str)
 
-        if ElfGame(game_id, game_sets).possible_game(game_bag):
+        elf_game = ElfGame(game_id, game_sets)
+
+        if elf_game.possible_game(game_bag):
             id_sum += game_id
+
+        power_sum += elf_game.calc_power()
 
     print(f"Given the Bag: 12 red cubes, 13 green cubes, 14 blue cubes\n")
     print(f"The sum of the IDs of all possible games = {id_sum}\n")
+    print(f"The sum of the power of all games = {power_sum}\n")
