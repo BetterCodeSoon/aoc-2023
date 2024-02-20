4b4f6010ceea3f6585022af438cb7c1a5d0ffb8c
BetterCodeSoon <bettercodesoon@proton.me>
2024-02-20 13:09:27 +0100

    Added evaluate game function to check if a game is possible with the given bag.

--- src/aoc2/aoc2.py
+++ src/aoc2/aoc2.py
@@ -1,4 +1,11 @@
 import src.utils.file_helper as file_helper
+from aoc2.bag import Bag
+from aoc2.elf_game import ElfGame
+
+
+def evaluate_game(elf_game: ElfGame, bag: Bag) -> bool:
+    return elf_game.possible_game(bag)
+
 
 if __name__ == '__main__':
     puzzle_path = file_helper.puzzle_input_path(2, 1)
