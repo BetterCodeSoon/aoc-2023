c9fa1ada0bbe5697ec904314ed0410b9ef37b3eb
BetterCodeSoon <bettercodesoon@proton.me>
2024-02-20 13:52:36 +0100

    Correct solution for the first part of Day2 puzzle where the puzzle input is read from a txt file.

--- src/aoc2/aoc2.py
+++ src/aoc2/aoc2.py
@@ -1,6 +1,7 @@
 import src.utils.file_helper as file_helper
-from aoc2.bag import Bag
-from aoc2.elf_game import ElfGame
+import src.aoc2.input_helper as input_helper
+from src.aoc2.bag import Bag
+from src.aoc2.elf_game import ElfGame
 
 
 def evaluate_game(elf_game: ElfGame, bag: Bag) -> bool:
@@ -8,7 +9,18 @@ def evaluate_game(elf_game: ElfGame, bag: Bag) -> bool:
 
 
 if __name__ == '__main__':
+    id_sum = 0
+    game_bag = Bag(12, 13, 14)
     puzzle_path = file_helper.puzzle_input_path(2, 1)
     file_lines = file_helper.read_file_lines(puzzle_path)
 
-    # toDo Puzzle Solution
+    for line in file_lines:
+        game_str, game_sets_str = line.rstrip("\n").split(": ")
+        game_id = input_helper.read_game_id(game_str)
+        game_sets = input_helper.read_game_sets_list(game_sets_str)
+
+        if ElfGame(game_id, game_sets).possible_game(game_bag):
+            id_sum += game_id
+
+    print(f"Given the Bag: 12 red cubes, 13 green cubes, 14 blue cubes\n")
+    print(f"The sum of the IDs of all possible games = {id_sum}\n")
