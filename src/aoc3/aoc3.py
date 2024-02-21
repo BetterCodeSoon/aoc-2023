0a5ab5a7b68d79cbeb536fb1870657d57fa186f3
BetterCodeSoon <bettercodesoon@proton.me>
2024-02-21 21:25:42 +0100

    Initial project structure with puzzle input and example test cases.

---
+++ src/aoc3/aoc3.py
@@ -0,0 +1,7 @@
+from src.utils import file_helper
+
+if __name__ == '__main__':
+    puzzle_path = file_helper.puzzle_input_path(3, 1)
+    file_lines = file_helper.read_file_lines(puzzle_path)
+
+    # toDo: here Solution for Day3 puzzle
