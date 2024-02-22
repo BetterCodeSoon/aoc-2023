c86bb8370e48a0885f7ea673f0fbd7d07ffaba5a
BetterCodeSoon <bettercodesoon@proton.me>
2024-02-22 12:49:41 +0100

    New utility class to extract the expected values for testcases from a given input str.

---
+++ src/utils/input/input_line.py
@@ -0,0 +1,32 @@
+class InputLine:
+    expected_values_list: [str]
+    has_expected_values: bool = False
+    line_str: str
+
+    def __init__(self, input_str: str, delimiter: str = '| ', value_separator: str = ', ',
+                 strip_trailing_spaces: bool = True):
+        if input_str is None:
+            raise Exception(f"input string is None")
+
+        expected_str = None
+        if delimiter in input_str:
+            self.line_str, expected_str = input_str.split(delimiter)
+        else:
+            self.line_str = input_str
+
+        if expected_str is not None and len(expected_str) > 0:
+            expected_values = expected_str.split(value_separator)
+            if strip_trailing_spaces:
+                expected_values = [value.strip() for value in expected_values]
+            self.expected_values_list = expected_values
+            self.has_expected_values = True
+        else:
+            self.expected_values_list = []
+
+    def __eq__(self, other):
+        return (self.line_str == other.line_str
+                and self.expected_values_list == other.expected_values_list
+                and self.has_expected_values == other.has_expected_values)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
