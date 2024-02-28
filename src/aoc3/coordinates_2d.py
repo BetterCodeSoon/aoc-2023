88443cb1b37b25f422dc04bc4124fc3885f19ddc
BetterCodeSoon <bettercodesoon@proton.me>
2024-02-28 21:16:57 +0100

    Small refactor to improve class naming and short version for imports.

--- src/aoc3/coordinates_2d.py
+++ src/aoc3/coordinates_2d.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Coordinates2D:
+class Coordinates2d:
     x: int
     y: int
 
--- tests/aoc3/test_coordinates.py
+++ tests/aoc3/test_coordinates.py
@@ -1,16 +1,16 @@
 import pytest
 
-from src.aoc3.coordinates_2d import Coordinates2D
+from src.aoc3.coordinates_2d import Coordinates2d
 
 
 class TestCoordinates:
 
-    @pytest.mark.parametrize("coord1, coord2, expected", [(Coordinates2D(1, 1), Coordinates2D(1, 1), True),
-                                                          (Coordinates2D(1, 1), Coordinates2D(2, 2), False)])
+    @pytest.mark.parametrize("coord1, coord2, expected", [(Coordinates2d(1, 1, '.'), Coordinates2d(1, 1, '.'), True),
+                                                          (Coordinates2d(1, 1, '.'), Coordinates2d(2, 2, '.'), False)])
     def test_equality(self, coord1, coord2, expected):
         assert (coord1 == coord2) == expected
 
-    @pytest.mark.parametrize("coord1, coord2, expected", [(Coordinates2D(1, 1), Coordinates2D(1, 1), False),
-                                                          (Coordinates2D(1, 1), Coordinates2D(2, 2), True)])
+    @pytest.mark.parametrize("coord1, coord2, expected", [(Coordinates2d(1, 1, '.'), Coordinates2d(1, 1, '.'), False),
+                                                          (Coordinates2d(1, 1, '.'), Coordinates2d(2, 2, '.'), True)])
     def test_inequality(self, coord1, coord2, expected):
         assert (coord1 != coord2) == expected
