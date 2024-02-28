f067cbdcc83b9a2563ccb710c8901c11a5e06b3b
BetterCodeSoon <bettercodesoon@proton.me>
2024-02-28 23:07:56 +0100

    Fixed Coordinates class tests.

--- tests/aoc3/test_coordinates.py
+++ tests/aoc3/test_coordinates.py
@@ -5,12 +5,12 @@ from src.aoc3.coordinates_2d import Coordinates2d
 
 class TestCoordinates:
 
-    @pytest.mark.parametrize("coord1, coord2, expected", [(Coordinates2d(1, 1, '.'), Coordinates2d(1, 1, '.'), True),
-                                                          (Coordinates2d(1, 1, '.'), Coordinates2d(2, 2, '.'), False)])
+    @pytest.mark.parametrize("coord1, coord2, expected", [(Coordinates2d(1, 1, ), Coordinates2d(1, 1), True),
+                                                          (Coordinates2d(1, 1, ), Coordinates2d(2, 2), False)])
     def test_equality(self, coord1, coord2, expected):
         assert (coord1 == coord2) == expected
 
-    @pytest.mark.parametrize("coord1, coord2, expected", [(Coordinates2d(1, 1, '.'), Coordinates2d(1, 1, '.'), False),
-                                                          (Coordinates2d(1, 1, '.'), Coordinates2d(2, 2, '.'), True)])
+    @pytest.mark.parametrize("coord1, coord2, expected", [(Coordinates2d(1, 1), Coordinates2d(1, 1), False),
+                                                          (Coordinates2d(1, 1), Coordinates2d(2, 2), True)])
     def test_inequality(self, coord1, coord2, expected):
         assert (coord1 != coord2) == expected
