58c2fc4f67e437a4c8c748623a03d798445c34ed
BetterCodeSoon <bettercodesoon@proton.me>
2024-03-01 12:01:40 +0100

    Fixed and improved SchematicNumber class with additional helper function to add a neighbor digit and tests asserting correct exceptions for wrong inputs.

--- src/aoc3/schematic_number.py
+++ src/aoc3/schematic_number.py
@@ -10,26 +10,37 @@ class SchematicNumber:
         if len(cells) < 1:
             raise ValueError("A number must contain at least one digit")
 
-        x = cells[0].coords.x
-        for cell in cells:
-            if cell.coords.x != x:
-                raise ValueError(
-                    f"A number can only be formed by digits on the same row.\n"
-                    f"But for cell: {cell} coords.x = {cell.coords.x} instead of {x}\n")
-            if not cell.is_digit():
-                raise ValueError(
-                    f"A cell belonging to a number needs to contain a digit and not cell.elem: {cell.element}")
+        self.row_y = cells[0].coords.y
+        [self._validate_cell(cell) for cell in cells]
 
         self.cells: [Cell] = cells
         self.value: int = int("".join(cell.element for cell in cells))
 
+    def _validate_cell(self, cell: Cell):
+        if cell.coords.y != self.row_y:
+            raise ValueError(
+                f"A number can only be formed by digits on the same row.\n"
+                f"But for cell: {cell} coords.y = {cell.coords.y} instead of {self.row_y}\n")
+        if not cell.is_digit():
+            raise ValueError(
+                f"A cell belonging to a number needs to contain a digit and not cell.elem: {cell.element}")
+
+    def add_cell(self, cell: Cell):
+        # only allow to add number cell right next to the existing cell
+        if cell.coords.x != (self.cells[-1].coords.x + 1):
+            raise ValueError(f"It is only possible to add a cell with digit to the right of an existing digit!\n"
+                             f"Last cell x-coord: {self.cells[-1].coords.x}\n"
+                             f"Cell that is supposed to be added x-coord: {cell.coords.x}")
+        self._validate_cell(cell)
+        self.cells.append(cell)
+
     def __eq__(self, other):
         if not isinstance(other, type(self)):
             raise Exception(f"Cannot compare other of type: {type(other)} to class type: {type(self)}")
-        return self.cells == other.cells and self.value == other.value
+        return self.row_y == self.cells == other.cells and self.value == other.value
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
     def __hash__(self):
-        return hash((tuple(self.cells), self.value))
+        return hash((self.row_y, tuple(self.cells), self.value))
--- tests/aoc3/test_schematic_number.py
+++ tests/aoc3/test_schematic_number.py
@@ -5,18 +5,57 @@ from src.aoc3.schematic_number import SchematicNumber
 from tests.aoc3.test_cell import create_cell
 
 
-def cell_list(number_str: str) -> [Cell]:
-    return [create_cell(0, y, digit) for y, digit in enumerate(number_str)]
+def number_cell_list(number_str: str) -> [Cell]:
+    return [create_cell(x, 0, digit) for x, digit in enumerate(number_str)]
 
 
 class TestSchematicNumber:
 
     @pytest.mark.parametrize("cells, expected_value", [
-        (cell_list("1"), 1),
-        (cell_list("123"), 123)
+        (number_cell_list("1"), 1),
+        (number_cell_list("123"), 123)
     ])
     def test_initialization(self, cells, expected_value):
         assert SchematicNumber(cells).value == expected_value
+
+    def test_add_cell(self):
+        expected_cells = number_cell_list("12")
+        cell1 = expected_cells[0]
+        cell2 = expected_cells[1]
+        number = SchematicNumber([cell1])
+        number.add_cell(cell2)
+        assert number.cells == expected_cells
+
+    @pytest.mark.parametrize("expected_cells, coord_x, expected_error", [
+        (number_cell_list("12"), 0, None),
+        (number_cell_list("12"), 1, ValueError)
+    ])
+    def test_add_cell(self, expected_cells, coord_x, expected_error):
+        cell1: Cell = expected_cells[0]
+        cell2: Cell = expected_cells[1]
+        cell2.coords.x += coord_x
+        number = SchematicNumber([cell1])
+        if expected_error is None:
+            assert number.add_cell(cell2) is None
+            assert number.cells == expected_cells
+        else:
+            with pytest.raises(expected_error):
+                number.add_cell(cell2)
+
+    @pytest.mark.parametrize("y, element, number_row_y, expected", [
+        (2, '5', 2, None),  # Test case for same row and digit
+        (3, '7', 2, ValueError),  # Test case for different row
+        (2, '#', 2, ValueError)  # Test case for non-digit element
+    ])
+    def test_validate_cell(self, y, element, number_row_y, expected):
+        number = SchematicNumber([create_cell(0, 2, "1")])
+        cell = create_cell(1, y, element)
+
+        if expected is None:
+            assert number._validate_cell(cell) is expected
+        else:
+            with pytest.raises(expected):
+                number._validate_cell(cell)
 
     @pytest.mark.parametrize("cell1, cell2, expected", [
         (create_cell(1, 1, "."), create_cell(1, 1, "."), True),
@@ -33,7 +72,8 @@ class TestSchematicNumber:
         assert (cell1 != cell2) == expected
 
     def test_hash(self):
+        row_y = 0
         value = 1234
-        cells = cell_list(str(value))
+        cells = number_cell_list(str(value))
         number = SchematicNumber(cells)
-        assert number.__hash__() == hash((tuple(cells), value))
+        assert number.__hash__() == hash((row_y, tuple(cells), value))
