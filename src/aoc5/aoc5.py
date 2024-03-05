b92a1c7157f340a281b35732f8586196568894b5
BetterCodeSoon <bettercodesoon@proton.me>
2024-03-05 18:19:51 +0100

    Initial day5 project structure and input files.

---
+++ src/aoc5/aoc5.py
@@ -0,0 +1,6 @@
+from src.utils.input.puzzle_input_file import PuzzleInputFile
+
+if __name__ == '__main__':
+    puzzle_file = PuzzleInputFile(5, 1, "#")
+
+    # toDo: solution for day5
---
+++ tests/aoc5/test_aoc5.py
@@ -0,0 +1,5 @@
+class TestAoc5:
+
+    def test_aoc5(self):
+        # toDo test for day5 solution
+        pass
