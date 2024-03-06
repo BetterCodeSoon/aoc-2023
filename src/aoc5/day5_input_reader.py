1e08d5e59c40ebdd57196937325c38040f6c8d69
BetterCodeSoon <bettercodesoon@proton.me>
2024-03-06 01:05:57 +0100

    Day5InputReader class and tests to read in seeds and maps from input files.

---
+++ src/aoc5/day5_input_reader.py
@@ -0,0 +1,39 @@
+from src.utils.input.input_file import InputFile
+from utils.string_helper import to_int
+
+
+class Day5InputReader:
+
+    def __init__(self, input_file: InputFile):
+        if input_file is None:
+            raise ValueError("Danger eek! The input file is None!!\n")
+
+        file_lines = input_file.file_lines
+        self.seeds: [int] = self.seeds(file_lines[0])
+
+        maps_lines: [str] = file_lines[2:]
+        self.maps_str_list = self.collect_maps(maps_lines)
+
+    @staticmethod
+    def seeds(seeds_str: str) -> [int]:
+        return to_int(seeds_str.split(":")[1].lstrip().split(" "))
+
+    @staticmethod
+    def collect_maps(maps_lines: [str]) -> [[str]]:
+        last_line_index = len(maps_lines) - 1
+        maps_str_list: [[str]] = []
+        current_map_list = []
+
+        for index, line in enumerate(maps_lines):
+            if line[0].isalpha():
+                # map title
+                current_map_list.append(line.rstrip()[:-5])
+            if line[0].isdigit():
+                # source, destination numbers
+                current_map_list.append(line.rstrip())
+            if line[0] == "\n" or index == last_line_index:
+                # end of the map so save it and reset current map
+                maps_str_list.append(current_map_list)
+                current_map_list = []
+
+        return maps_str_list
---
+++ tests/aoc5/test_day5_input_reader.py
@@ -0,0 +1,26 @@
+import pytest
+
+from src.aoc5.day5_input_reader import Day5InputReader
+from tests.aoc5 import tcase1_file_lines
+
+
+class TestDay5InputReader:
+
+    @pytest.mark.parametrize("input_str, expected_seeds_list", [
+        ("seeds: 79 14 55 13", [79, 14, 55, 13])
+    ])
+    def test_seeds(self, input_str, expected_seeds_list):
+        assert Day5InputReader.seeds(input_str) == expected_seeds_list
+
+    @pytest.mark.parametrize("input_lines, expected_maps_list", [
+        (tcase1_file_lines()[2:],
+         [['seed-to-soil', '50 98 2', '52 50 48'],
+          ['soil-to-fertilizer', '0 15 37', '37 52 2', '39 0 15'],
+          ['fertilizer-to-water', '49 53 8', '0 11 42', '42 0 7', '57 7 4'],
+          ['water-to-light', '88 18 7', '18 25 70'],
+          ['light-to-temperature', '45 77 23', '81 45 19', '68 64 13'],
+          ['temperature-to-humidity', '0 69 1', '1 0 69'],
+          ['humidity-to-location', '60 56 37', '56 93 4']])
+    ])
+    def test_collect_maps(self, input_lines, expected_maps_list):
+        assert Day5InputReader.collect_maps(input_lines) == expected_maps_list
