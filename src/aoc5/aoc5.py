8c3e274ea9ec127702dbe30c2249e91c9b3360d3
BetterCodeSoon <bettercodesoon@proton.me>
2024-03-09 21:50:04 +0100

    Correct solution for Day5 part one.

--- src/aoc5/aoc5.py
+++ src/aoc5/aoc5.py
@@ -1,6 +1,11 @@
-from src.utils.input.puzzle_input_file import PuzzleInputFile
+from src import TYPE_PUZZLE
+from src.aoc5.almanac import Almanac
+from src.aoc5.day5_input_reader import Day5InputReader
+from src.utils.input.input_file import InputFile
 
 if __name__ == '__main__':
-    puzzle_file = PuzzleInputFile(5, 1, "#")
+    puzzle_file = InputFile(TYPE_PUZZLE, 5, 1, "#", False)
+    input_reader = Day5InputReader(puzzle_file)
 
-    # toDo: solution for day5
+    print(f"Lowest location number = "
+          f"{Almanac(input_reader.seeds, input_reader.maps_str_list).lowest_location_number()} \n")
--- tests/aoc5/test_aoc5.py
+++ tests/aoc5/test_aoc5.py
@@ -1,5 +1,15 @@
+from src import TYPE_PUZZLE
+from aoc5.almanac import Almanac
+from aoc5.day5_input_reader import Day5InputReader
+from utils.input.input_file import InputFile
+
+
 class TestAoc5:
 
     def test_aoc5(self):
-        # toDo test for day5 solution
-        pass
+        expected_lowest_location_number = 457535844
+
+        puzzle_file = InputFile(TYPE_PUZZLE, 5, 1, "#", False)
+        input_reader = Day5InputReader(puzzle_file)
+        assert Almanac(input_reader.seeds,
+                       input_reader.maps_str_list).lowest_location_number() == expected_lowest_location_number
