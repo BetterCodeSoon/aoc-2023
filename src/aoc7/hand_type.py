759dad9b11f45b4c2555a0660d4ffec3ed03dbb8
BetterCodeSoon <bettercodesoon@proton.me>
2024-03-15 00:21:00 +0100

    Fixed typo and shortened constant access names.

--- src/aoc7/hand_type.py
+++ src/aoc7/hand_type.py
@@ -7,6 +7,6 @@ class HandType:
     FOUR_KIND = "Four of a kind"
     FULL_HOUSE = "Full House"
     THREE_KIND = "Three of a kind"
-    TWO_PAIRS = "Two pair"
+    TWO_PAIR = "Two pair"
     ONE_PAIR = "One pair"
     HIGH_CARD = "High card"
