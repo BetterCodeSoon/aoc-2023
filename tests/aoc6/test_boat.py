326d89bb26520738d05cab2bdb9cdf03b73b45c6
BetterCodeSoon <bettercodesoon@proton.me>
2024-03-14 12:35:13 +0100

    Refactor to simplify the Boat class.

--- tests/aoc6/test_boat.py
+++ tests/aoc6/test_boat.py
@@ -11,15 +11,4 @@ class TestBoat:
         (3, 7, 12)
     ])
     def test_calc_distance(self, time_button_pressed, max_race_duration, expected):
-        assert Boat._calc_distance(time_button_pressed, max_race_duration) == expected
-
-    @pytest.mark.parametrize("time_button_pressed, max_race_duration, expected_distance", [
-        (1, 7, 6),
-        (2, 7, 10),
-        (3, 7, 12)
-    ])
-    def test_hold_button(self, time_button_pressed, max_race_duration, expected_distance):
-        boat = Boat(max_race_duration)
-        boat.hold_button(time_button_pressed)
-        assert boat.speed == time_button_pressed
-        assert boat.distance_traveled == expected_distance
+        assert Boat.calc_distance(time_button_pressed, max_race_duration) == expected
