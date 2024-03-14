43e7943b4582c19f3a6615ca6974891b787a1672
BetterCodeSoon <bettercodesoon@proton.me>
2024-03-14 12:36:58 +0100

    Correct solution for Day6 part1.

--- src/aoc6/aoc6.py
+++ src/aoc6/aoc6.py
@@ -1,7 +1,12 @@
-from src import TYPE_PUZZLE
-from src.utils.input.input_file import InputFile
+from src.aoc6.boat_race import BoatRace
 
 if __name__ == '__main__':
-    puzzle_file = InputFile(TYPE_PUZZLE, 6, 1, "#", False)
+    # Puzzle Input
+    # Time:        35     93     73     66
+    # Distance:   212   2060   1201   1044
+    max_durations = [35, 93, 73, 66]
+    distance_records = [212, 2060, 1201, 1044]
 
-    # toDo: Solution for Day 6
+    part1 = BoatRace.error_margins_for_race(max_durations, distance_records)
+
+    print(f"The product of all number of ways to beat each race = {part1}\n")
--- tests/aoc6/test_aoc6.py
+++ tests/aoc6/test_aoc6.py
@@ -1,7 +1,10 @@
-from src import TYPE_PUZZLE
+from src.aoc6.boat_race import BoatRace
 
 
 class TestAoc6:
 
     def test_aoc6(self):
-        pass
+        expected_part1 = 114400
+        max_durations = [35, 93, 73, 66]
+        distance_records = [212, 2060, 1201, 1044]
+        assert BoatRace.error_margins_for_race(max_durations, distance_records) == expected_part1
