3682ed0d17de4930da8265850aaf3e46ba987f14
BetterCodeSoon <bettercodesoon@proton.me>
2024-03-14 13:15:41 +0100

    Fixing typo in function signature.

--- src/aoc6/boat_race.py
+++ src/aoc6/boat_race.py
@@ -6,7 +6,7 @@ from src.aoc6.boat import Boat
 class BoatRace:
 
     @staticmethod
-    def _ways_to_beat_distance_record(max_race_duration: int, distance_record: int, ) -> [int]:
+    def _ways_to_beat_distance_record(max_race_duration: int, distance_record: int) -> [int]:
         ways_to_beat_record: [int] = []
 
         for time_button_pressed in range(0, distance_record):
