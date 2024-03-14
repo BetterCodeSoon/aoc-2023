ee960153f4720b77a3b5ed5d0b8629a63ab49598
BetterCodeSoon <bettercodesoon@proton.me>
2024-03-14 13:55:16 +0100

    Correct solution for Day6 part2.

--- src/aoc6/aoc6.py
+++ src/aoc6/aoc6.py
@@ -9,4 +9,10 @@ if __name__ == '__main__':
 
     part1 = BoatRace.error_margins_for_race(max_durations, distance_records)
 
-    print(f"The product of all number of ways to beat each race = {part1}\n")
+    print(f"The product of all number of ways to beat each race = {part1}\n\n")
+
+    # time: 35937366
+    # distance: 212206012011044
+    part2 = BoatRace.ways_by_min_max(35937366, 212206012011044)
+
+    print(f"The race can be beat in {part2} number of ways!\n")
