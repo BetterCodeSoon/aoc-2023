5c6e5eb55c98e6951061eb89b4478a19bc3afe3a
BetterCodeSoon <bettercodesoon@proton.me>
2024-03-15 12:44:40 +0100

    Correct solution for day7 part one.

--- src/aoc7/aoc7.py
+++ src/aoc7/aoc7.py
@@ -1,3 +1,8 @@
+from src.aoc7.camel_card_game import CamelCardGame
+from src.utils.input.puzzle_input_file import PuzzleInputFile
+
 if __name__ == '__main__':
-    # toDo: solution for day7
-    pass
+    puzzle_file = PuzzleInputFile(7, 1, '#', " ")
+    card_game = CamelCardGame(puzzle_file.puzzle_input_lines)
+
+    print(f"The total winnings are = {card_game.total_winnings} \n\n")
--- tests/aoc7/test_aoc7.py
+++ tests/aoc7/test_aoc7.py
@@ -1,3 +1,11 @@
+from src.aoc7.camel_card_game import CamelCardGame
+from src.utils.input.puzzle_input_file import PuzzleInputFile
+
+
 class TestAoc7:
-    # toDo: tests for aoc7 solution
-    pass
+
+    def test_aoc7(self):
+        puzzle_file = PuzzleInputFile(7, 1, '#', " ")
+        card_game = CamelCardGame(puzzle_file.puzzle_input_lines)
+        # Part 1:
+        assert card_game.total_winnings == 248105065
