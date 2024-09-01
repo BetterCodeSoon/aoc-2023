e7a548c01e04eab837df16ca9141442114f963b8
BetterCodeSoon <bettercodesoon@proton.me>
2024-09-01 17:22:20 +0200

    Initial project structure and puzzle input.

---
+++ src/aoc8/aoc8.py
@@ -0,0 +1,5 @@
+from src.utils.input.puzzle_input_file import PuzzleInputFile
+
+if __name__ == '__main__':
+    puzzle_file = PuzzleInputFile(8, 1, '#', " ")
+    # Day8 Solution
---
+++ tests/aoc8/test_aoc8.py
@@ -0,0 +1,9 @@
+from src.utils.input.puzzle_input_file import PuzzleInputFile
+
+
+class TestAoc8:
+
+    def test_aoc8(self):
+        puzzle_file = PuzzleInputFile(8, 1, '#', " ")
+
+        # Day8 test
