9ef90ab115b09cf09d576f7784573b0571c65c00
BetterCodeSoon <bettercodesoon@proton.me>
2024-12-27 18:30:01 +0100

    Correct solution for part1 and its test.

--- src/aoc8/aoc8.py
+++ src/aoc8/aoc8.py
@@ -1,5 +1,14 @@
+from src.aoc8.network import Network
+from src.aoc8.day8_input_reader import Day8InputReader
 from src.utils.input.puzzle_input_file import PuzzleInputFile
 
 if __name__ == '__main__':
-    puzzle_file = PuzzleInputFile(8, 1, '#', " ")
-    # Day8 Solution
+    puzzle_file = PuzzleInputFile(8, 1, '#', "")
+    day8_input_reader = Day8InputReader(puzzle_file)
+
+    # Day8 Solution Part 1:
+
+    network = Network(day8_input_reader.path, day8_input_reader.network_dict)
+    steps = network.steps_to_zzz()
+
+    print(f"It took {steps} to reach ZZZ! \n\n")
