39cdb73744c63a170a734400d1694984010501c9
BetterCodeSoon <bettercodesoon@proton.me>
2025-01-01 13:22:59 +0100

    Moves solutions for part 1 and 2 to methods. Added runtime benchmark to the calculations.

--- src/aoc8/aoc8.py
+++ src/aoc8/aoc8.py
@@ -1,14 +1,31 @@
-from src.aoc8.network import Network
+from deprecated import deprecated
+
 from src.aoc8.day8_input_reader import Day8InputReader
+from src.aoc8.network import Network
+from src.utils import benchmark_helper
 from src.utils.input.puzzle_input_file import PuzzleInputFile
+
+
+def solve_part1(input_reader: Day8InputReader):
+    # Day8 Solution Part 1:
+    network_1 = Network(input_reader.path, input_reader.network_dict)
+    steps = network_1.part1_steps_to_zzz()
+    print(f"Part1: It took {steps} to reach ZZZ! \n\n")
+
+
+@deprecated(reason="The brute force solution will get entangled in cycles and takes forever to calc")
+def solve_part2_brute_force_threadpool(input_reader: Day8InputReader):
+    # Day8 Solution Part 2:
+    network_2 = Network(input_reader.path, input_reader.network_dict)
+    print(f"\n\nPart2: Starting with {len(network_2.current_nodes)}-starting nodes ...\n\n")
+    count = network_2.calc_part2_brute_force_threadpool()
+    print(f"it took {count} steps to reach a state where all nodes end with Z\n\n"
+          f"Final Nodes:\n{network_2.current_nodes}")
+
 
 if __name__ == '__main__':
     puzzle_file = PuzzleInputFile(8, 1, '#', "")
     day8_input_reader = Day8InputReader(puzzle_file)
 
-    # Day8 Solution Part 1:
-
-    network = Network(day8_input_reader.path, day8_input_reader.network_dict)
-    steps = network.part1_steps_to_zzz()
-
-    print(f"It took {steps} to reach ZZZ! \n\n")
+    benchmark_helper.benchmark(solve_part1, day8_input_reader)
+    # benchmark_helper.benchmark(solve_part2_brute_force_threadpool, day8_input_reader)
